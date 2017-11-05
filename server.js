8b8974e5a05bf9f616b9c824c941510a1548edaf
Shawn Dust <shawn.dust@gmail.com>
2017-11-05 01:35:22 -0600

    make it better

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/sendmessage', function(req, res) 
+	{ 
+		var twilio = require('twilio');
+
+		var accountSid = 'AC76aa1d8c1f355eeecb3e14f0fb55a74d'; // Your Account SID from www.twilio.com/console
+	var authToken = '2ded5722dcda4f03fb9fc9069742bb44';   // Your Auth Token from www.twilio.com/console
+
+	var twilio = require('twilio');
+	var client = new twilio(accountSid, authToken);
+
+	client.messages.create({
+		body: 'Hello from Node',
+		to: '+13094531417',  // Text this number
+	        from: '+13128184945' // From a valid Twilio number
+	})
+	
+
+	.then(function(message){ 
+		
+		
+		console.log(message.sid)
+		res.send('SUCCESS!')
+
+	})
+})
+app.listen(process.ENV || 3000, () => console.log('Example app listening on port 3000!'))
