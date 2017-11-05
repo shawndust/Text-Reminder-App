5da89e65482dfcb3cc2100d3ae32f8b9a0b6678c
Shawn Dust <shawn.dust@gmail.com>
2017-11-05 03:09:49 -0600

    Updates

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require('express')
 const app = express()
 var path = require("path");
+var cors = require('cors')
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cors())
+
+var twilio = require('twilio');
+
+var accountSid = 'AC76aa1d8c1f355eeecb3e14f0fb55a74d'; // Your Account SID from www.twilio.com/console
+var authToken = '2ded5722dcda4f03fb9fc9069742bb44';   // Your Auth Token from www.twilio.com/console
+
+var twilio = require('twilio');
+var client = new twilio(accountSid, authToken);
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
 
-app.get('/sendmessage', function(req, res) 
+app.post('/sendmessage', function(req, res) 
 	{ 
-		var twilio = require('twilio');
 
-		var accountSid = 'AC76aa1d8c1f355eeecb3e14f0fb55a74d'; // Your Account SID from www.twilio.com/console
-	var authToken = '2ded5722dcda4f03fb9fc9069742bb44';   // Your Auth Token from www.twilio.com/console
-
-	var twilio = require('twilio');
-	var client = new twilio(accountSid, authToken);
-
-	client.messages.create({
-		body: 'Hello from Node',
-		to: '+13094531417',  // Text this number
+        console.log(req.body)
+	    client.messages.create({
+            body: 'Dear ' + req.body.name + ' please take ' + req.body.quantity + ' '+ req.body.brand + ' pills now.',
+		    to: '+1'+ req.body.cellNum,  // Text this number
 	        from: '+13128184945' // From a valid Twilio number
 	})
 	
